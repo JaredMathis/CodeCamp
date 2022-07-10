499a51f49c6fec32879712181ad40421b48c993f
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:57:54 -0400

    main npm_install_latest_mykro nilmy

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -2,5 +2,8 @@ import {ui_element} from "./../../node_modules/mykro/src/ui/element.mjs";
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_main() {
   await m_js_arguments_assert()(arguments);
-  await ui_element(document.body, "span");
+  alert('here')
+  let span = await ui_element(document.body, "span");
+  span.innerHTML = 'hi'
 }
+await cc_main();
