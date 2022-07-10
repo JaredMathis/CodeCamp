3d4b570245b91297e298cf8d9c07d11956c6d9ed
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:19:26 -0400

    main npm_install_latest_mykro nilmy

--- src/cc/lesson/console/log.mjs
+++ src/cc/lesson/console/log.mjs
@@ -1,4 +1,20 @@
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_console_log() {
   await m_js_arguments_assert()(arguments);
+  return {
+    examples: [
+      {
+        "input": "console.log('Hello')",
+        "output": `Hello`
+      },
+      {
+        "input": "console.log('Hello, my name is Simon Peter')",
+        "output": `Hello, my name is Simon Peter`
+      },
+      {
+        "input": "console.log('a')",
+        "output": `'a'`
+      }
+    ]
+  }
 }
