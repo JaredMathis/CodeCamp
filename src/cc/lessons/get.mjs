61a336ccd3a87c3f44d14647af15156547ca9775
JaredMathis <jared.mathis@native.consulting>
2022-07-10 19:06:23 -0400

    main file_js_auto cc_main

--- src/cc/lessons/get.mjs
+++ src/cc/lessons/get.mjs
@@ -1,4 +1,7 @@
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lessons_get() {
   await m_js_arguments_assert()(arguments);
+  return [
+    
+  ]
 }
--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -1,8 +1,10 @@
 import {ui_element_html_inner_set} from "./../../node_modules/mykro/src/ui/element/html/inner/set.mjs";
 import {ui_element} from "./../../node_modules/mykro/src/ui/element.mjs";
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+import {cc_lessons_get} from "./lessons/get.mjs";
 export async function cc_main() {
   await m_js_arguments_assert()(arguments);
+  let lessons = await cc_lessons_get();
   let span = await ui_element(document.body, "span");
   await ui_element_html_inner_set(span, "hi");
 }
