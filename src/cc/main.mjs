6ba38268a2644f0cb7ac8fd1c0f685736a602374
JaredMathis <jared.mathis@native.consulting>
2022-07-10 19:05:04 -0400

    main file_js_auto cc_main

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -1,8 +1,9 @@
+import {ui_element_html_inner_set} from "./../../node_modules/mykro/src/ui/element/html/inner/set.mjs";
 import {ui_element} from "./../../node_modules/mykro/src/ui/element.mjs";
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_main() {
   await m_js_arguments_assert()(arguments);
   let span = await ui_element(document.body, "span");
-  span.innerHTML = 'hi'
+  await ui_element_html_inner_set(span, "hi");
 }
 await cc_main();
