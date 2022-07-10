f540d976aeec89811af7dc810ea16915b4562f56
JaredMathis <jared.mathis@native.consulting>
2022-07-10 19:00:16 -0400

    main noop remove alert

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -2,7 +2,6 @@ import {ui_element} from "./../../node_modules/mykro/src/ui/element.mjs";
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_main() {
   await m_js_arguments_assert()(arguments);
-  alert('here')
   let span = await ui_element(document.body, "span");
   span.innerHTML = 'hi'
 }
