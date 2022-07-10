5c7e3585c7225dcb6f60c3cb1c9e9625efd9bea2
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:47:25 -0400

    main npm_install_latest_mykro nilmy

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -1,4 +1,5 @@
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_main() {
   await m_js_arguments_assert()(arguments);
+  document.body.innerHTML = 'hi'
 }
