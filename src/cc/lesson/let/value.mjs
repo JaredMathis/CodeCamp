248457d3085f245a72211e7732dab195546d8f1c
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:57:56 -0400

    main file_js_auto cc_lesson_let_value

--- src/cc/lesson/let/value.mjs
+++ src/cc/lesson/let/value.mjs
@@ -1,5 +1,4 @@
-import {m_js_string_join} from "./../../../../node_modules/mykro/src/m/js/string/join.mjs";
-import {random_list_slice} from "./../../../../node_modules/mykro/src/random/list/slice.mjs";
+import {random_list_slice_join} from "./../../../../node_modules/mykro/src/random/list/slice/join.mjs";
 import {cc_tokens_get} from "./../../tokens/get.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_let_value() {
@@ -8,8 +7,7 @@ export async function cc_lesson_let_value() {
   return {
     name: "let value; value = ...;",
     example_get: async () => {
-      let shortened = await random_list_slice(tokens, 3);
-      let joined = await m_js_string_join(shortened, " ");
+      let joined = await random_list_slice_join(tokens, 3, " ");
       return {
         input: `let value;
 value = '${joined}';
