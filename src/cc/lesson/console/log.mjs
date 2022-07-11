79cd2aa47282a22ae10ac57b3c405871d5bda7f3
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:35:22 -0400

    main noop progress

--- src/cc/lesson/console/log.mjs
+++ src/cc/lesson/console/log.mjs
@@ -3,7 +3,7 @@ import {list_map} from "./../../../../node_modules/mykro/src/list/map.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_console_log() {
   await m_js_arguments_assert()(arguments);
-  let verse = `For God so loved the world that he gave his only Son, so that everyone who believes in him may not perish but may have eternal life.`;
+  let verse = `For God so loved the world that he gave his only Son so that everyone who believes in him may not perish but may have eternal life`;
   let tokens = m_js_string_split(verse, " ");
   return {
     name: "console.log(...);",
