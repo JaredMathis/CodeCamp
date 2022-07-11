ec47622c3adede76788e348bb88bab662f21c376
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:03:00 -0400

    main noop unused import

--- src/cc/lesson/console/log.mjs
+++ src/cc/lesson/console/log.mjs
@@ -1,7 +1,6 @@
 import {random_list_slice} from "./../../../../node_modules/mykro/src/random/list/slice.mjs";
 import {m_js_string_join} from "./../../../../node_modules/mykro/src/m/js/string/join.mjs";
 import {m_js_string_split} from "./../../../../node_modules/mykro/src/m/js/string/split.mjs";
-import {list_map} from "./../../../../node_modules/mykro/src/list/map.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_console_log() {
   await m_js_arguments_assert()(arguments);
