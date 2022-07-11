c079b349b2a6ade7158d48b87b9524be08a9a7b4
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:07:54 -0400

    main file_js_auto cc_lesson_console_log

--- src/cc/lesson/console/log.mjs
+++ src/cc/lesson/console/log.mjs
@@ -1,3 +1,4 @@
+import {list_take} from "./../../../../node_modules/mykro/src/list/take.mjs";
 import {m_js_string_join} from "./../../../../node_modules/mykro/src/m/js/string/join.mjs";
 import {list_join} from "./../../../../node_modules/mykro/src/list/join.mjs";
 import {list_starting_at} from "./../../../../node_modules/mykro/src/list/starting/at.mjs";
@@ -20,7 +21,8 @@ export async function cc_lesson_console_log() {
       let start_index = await random_number(0, max_selectable_index);
       let size = await random_number(1, max_words);
       let starting_at = await list_starting_at(tokens, start_index);
-      let joined = await m_js_string_join(starting_at, " ");
+      let shortened = await list_take(starting_at, size);
+      let joined = await m_js_string_join(shortened, " ");
       return {
         input: `console.log('${joined}');`,
         output: joined
