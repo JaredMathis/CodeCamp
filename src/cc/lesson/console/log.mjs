8b7891854a95448997e5a283d027bfc1293b83d0
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:12:48 -0400

    main file_js_auto cc_lesson_console_log

--- src/cc/lesson/console/log.mjs
+++ src/cc/lesson/console/log.mjs
@@ -1,3 +1,4 @@
+import {cc_tokens_get} from "./../../tokens/get.mjs";
 import {random_list_slice} from "./../../../../node_modules/mykro/src/random/list/slice.mjs";
 import {m_js_string_join} from "./../../../../node_modules/mykro/src/m/js/string/join.mjs";
 import {m_js_string_split} from "./../../../../node_modules/mykro/src/m/js/string/split.mjs";
@@ -11,14 +12,8 @@ export async function cc_lesson_console_log() {
       let shortened = await random_list_slice(tokens, 3);
       let joined = await m_js_string_join(shortened, " ");
       return {
-        input: `console.log('${joined}');`,
+        input: `console.log('${joined}');`
       };
     }
   };
 }
-async function cc_tokens_get() {
-  let verse = `For God so loved the world that he gave his only Son so that everyone who believes in him may not perish but may have eternal life`;
-  let tokens = await m_js_string_split(verse, " ");
-  return tokens;
-}
-
--- src/cc/tokens/get.mjs
+++ src/cc/tokens/get.mjs
@@ -1,4 +1,7 @@
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_tokens_get() {
   await m_js_arguments_assert()(arguments);
+  let verse = `For God so loved the world that he gave his only Son so that everyone who believes in him may not perish but may have eternal life`;
+  let tokens = await m_js_string_split(verse, " ");
+  return tokens;
 }
