099e90c25e2ad30085fb0f6af3282e58e1c39473
JaredMathis <jared.mathis@native.consulting>
2022-07-11 18:52:05 -0400

    main noop refactor

--- src/cc/lesson/console/log.mjs
+++ src/cc/lesson/console/log.mjs
@@ -7,6 +7,7 @@ import {list_size} from "./../../../../node_modules/mykro/src/list/size.mjs";
 import {m_js_string_split} from "./../../../../node_modules/mykro/src/m/js/string/split.mjs";
 import {list_map} from "./../../../../node_modules/mykro/src/list/map.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+import { list_max_index } from ".mykro/src/list/max/index.mjs";
 export async function cc_lesson_console_log() {
   await m_js_arguments_assert()(arguments);
   let verse = `For God so loved the world that he gave his only Son so that everyone who believes in him may not perish but may have eternal life`;
@@ -15,8 +16,7 @@ export async function cc_lesson_console_log() {
     name: "console.log(...);",
     example_get: async () => {
       let max_words = 3;
-      let tokens_size = await list_size(tokens);
-      let max_index = tokens_size - 1;
+      let max_index = await list_max_index(tokens);
       let max_selectable_index = max_index - (max_words - 1);
       let start_index = await random_number(0, max_selectable_index);
       let size = await random_number(1, max_words);
