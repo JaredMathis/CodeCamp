e1a85ccdbcac5ff19f0292bf781600f676891cd8
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:28:13 -0400

    main noop progress

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -5,12 +5,14 @@ import {m_js_for_each} from "./../../node_modules/mykro/src/m/js/for/each.mjs";
 import {ui_element_text} from "./../../node_modules/mykro/src/ui/element/text.mjs";
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 import {cc_lessons_get} from "./lessons/get.mjs";
+import { ui_element } from "mykro/src/ui/element.mjs";
 export async function cc_main(parent) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
   let lessons = await cc_lessons_get();
-  let span = await ui_element_text(parent, "ol", "Lessons");
+  let span = await ui_element_text(parent, "h1", "Lessons");
+  let list = ui_element(parent, "ul");
   await m_js_for_each(lessons, async lesson => {
-    await ui_element_text(parent, "li", await m_js_property_get(lesson, "name"));
+    await ui_element_text(list, "li", await m_js_property_get(lesson, "name"));
   });
 }
 await cc_main(document.body);
