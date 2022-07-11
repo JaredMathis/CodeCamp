86f8ef452e7ad925f42d64a00ae35971f3d32003
JaredMathis <jared.mathis@native.consulting>
2022-07-10 22:55:27 -0400

    main file_js_auto cc_ui_lesson

--- src/cc/ui/lesson.mjs
+++ src/cc/ui/lesson.mjs
@@ -3,10 +3,11 @@ import {ui_element_text} from "./../../../node_modules/mykro/src/ui/element/text
 import {ui_html_element_is} from "./../../../node_modules/mykro/src/ui/html/element/is.mjs";
 import {m_js_defined_is} from "./../../../node_modules/mykro/src/m/js/defined/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+import {m_js_property_get} from "./../../../node_modules/mykro/src/m/js/property/get.mjs";
 export async function cc_ui_lesson(parent, lesson) {
   await m_js_arguments_assert(ui_html_element_is, m_js_defined_is)(arguments);
   let container = await ui_element(parent, "div");
-  let span = await ui_element_text(container, "h1", "Lessons");
+  let span = await ui_element_text(container, "h1", "Lesson " + await m_js_property_get(lesson, "name"));
   return {
     container
   };
