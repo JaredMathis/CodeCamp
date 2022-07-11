444924b935166ffed52c65504ee166823ff2ddfd
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:42:00 -0400

    main file_js_auto cc_ui_lesson_example

--- src/cc/ui/lesson/example.mjs
+++ src/cc/ui/lesson/example.mjs
@@ -1,3 +1,4 @@
+import {list_join} from "./../../../../node_modules/mykro/src/list/join.mjs";
 import {list_add} from "./../../../../node_modules/mykro/src/list/add.mjs";
 import {list_size} from "./../../../../node_modules/mykro/src/list/size.mjs";
 import {m_js_equals} from "./../../../../node_modules/mykro/src/m/js/equals.mjs";
@@ -44,6 +45,7 @@ export async function cc_ui_lesson_example(parent, example_get, example_number, 
       }
       await list_add(wrong_answers, wrong_answer);
     });
+    let answers = await list_join([output], wrong_answers);
   } else {
     let next_text = `Another `;
     if (is_quiz) {
