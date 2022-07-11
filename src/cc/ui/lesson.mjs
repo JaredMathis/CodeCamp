3dc6eb7fa75fdc3691843d61ae3b931c3af1bc9d
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:01:23 -0400

    main file_js_auto cc_ui_lessons

--- src/cc/ui/lesson.mjs
+++ src/cc/ui/lesson.mjs
@@ -9,6 +9,7 @@ export async function cc_ui_lesson(parent, lesson, go_back) {
   await m_js_arguments_assert(ui_html_element_is, m_js_defined_is, m_js_function_is)(arguments);
   let container = await ui_element(parent, "div");
   let span = await ui_element_text(container, "h1", "Lesson " + await m_js_property_get(lesson, "name"));
+  let button= await ui_element_text(container, 'button', 'Back to Lessons');
   return {
     container
   };
