36c5ab9bdf72ccc6c2e82c30b8f8618c33ff4f10
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:17:16 -0400

    main file_js_auto cc_main

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -1,3 +1,4 @@
+import {json_to} from "./../../node_modules/mykro/src/json/to.mjs";
 import {m_js_for_each} from "./../../node_modules/mykro/src/m/js/for/each.mjs";
 import {ui_element_text} from "./../../node_modules/mykro/src/ui/element/text.mjs";
 import {ui_element_html_inner_set} from "./../../node_modules/mykro/src/ui/element/html/inner/set.mjs";
@@ -8,6 +9,8 @@ export async function cc_main() {
   await m_js_arguments_assert(parent)(arguments);
   let lessons = await cc_lessons_get();
   let span = await ui_element_text(parent, "span", "Lessons");
-  await m_js_for_each(lessons, lesson => {});
+  await m_js_for_each(lessons, async lesson => {
+    await ui_element_text(parent, await json_to(lesson));
+  });
 }
 await cc_main(document.body);
