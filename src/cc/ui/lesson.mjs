736d31bb95e408ad2f2d11b83be22a7041423b39
JaredMathis <jared.mathis@native.consulting>
2022-07-10 22:53:19 -0400

    main file_js_auto cc_ui_lesson

--- src/cc/ui/lesson.mjs
+++ src/cc/ui/lesson.mjs
@@ -1,4 +1,8 @@
+import {ui_element_text} from "./../../../node_modules/mykro/src/ui/element/text.mjs";
+import {ui_html_element_is} from "./../../../node_modules/mykro/src/ui/html/element/is.mjs";
+import {m_js_defined_is} from "mykro/src/m/js/defined/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function cc_ui_lesson() {
-  await m_js_arguments_assert()(arguments);
+export async function cc_ui_lesson(parent, lesson) {
+  await m_js_arguments_assert(ui_html_element_is, m_js_defined_is)(arguments);
+  let span = await ui_element_text(container, "h1", "Lessons");
 }
