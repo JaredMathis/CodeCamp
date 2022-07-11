b0f424ee27abe9d603672176159ca99df2ff0c1f
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:16:35 -0400

    main file_js_auto cc_main

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -1,3 +1,5 @@
+import {m_js_for_each} from "./../../node_modules/mykro/src/m/js/for/each.mjs";
+import {ui_element_text} from "./../../node_modules/mykro/src/ui/element/text.mjs";
 import {ui_element_html_inner_set} from "./../../node_modules/mykro/src/ui/element/html/inner/set.mjs";
 import {ui_element} from "./../../node_modules/mykro/src/ui/element.mjs";
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
@@ -5,10 +7,7 @@ import {cc_lessons_get} from "./lessons/get.mjs";
 export async function cc_main() {
   await m_js_arguments_assert(parent)(arguments);
   let lessons = await cc_lessons_get();
-  let span = await ui_element(parent, "span");
-  await ui_element_html_inner_set(span, "Lessons");
-  await m_js_for_each(lessons, lesson => {
-    
-  })
+  let span = await ui_element_text(parent, "span", "Lessons");
+  await m_js_for_each(lessons, lesson => {});
 }
 await cc_main(document.body);
