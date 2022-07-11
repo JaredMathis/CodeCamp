73cf9a5a697eefba540c24775d95c3c2f6f3f0f7
JaredMathis <jared.mathis@native.consulting>
2022-07-11 18:52:19 -0400

    main file_js_auto cc_lesson_console_log

--- src/cc/lesson/console/log.mjs
+++ src/cc/lesson/console/log.mjs
@@ -7,7 +7,7 @@ import {list_size} from "./../../../../node_modules/mykro/src/list/size.mjs";
 import {m_js_string_split} from "./../../../../node_modules/mykro/src/m/js/string/split.mjs";
 import {list_map} from "./../../../../node_modules/mykro/src/list/map.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-import { list_max_index } from ".mykro/src/list/max/index.mjs";
+import {list_max_index} from "./../../../../node_modules/mykro/src/list/max/index.mjs";
 export async function cc_lesson_console_log() {
   await m_js_arguments_assert()(arguments);
   let verse = `For God so loved the world that he gave his only Son so that everyone who believes in him may not perish but may have eternal life`;
