cec0898e13b913358473e0b2e759fa99a2742aac
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:45:21 -0400

    main file_js_auto cc_lesson_let_value_twice

--- src/cc/lesson/let/value/twice.mjs
+++ src/cc/lesson/let/value/twice.mjs
@@ -1,4 +1,20 @@
+import {m_js_string_join} from "./../../../../../node_modules/mykro/src/m/js/string/join.mjs";
+import {random_list_slice} from "./../../../../../node_modules/mykro/src/random/list/slice.mjs";
+import {cc_tokens_get} from "./../../../tokens/get.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_let_value_twice() {
   await m_js_arguments_assert()(arguments);
+  let tokens = await cc_tokens_get();
+  return {
+    name: "let value; value = ...;",
+    example_get: async () => {
+      let shortened = await random_list_slice(tokens, 3);
+      let joined = await m_js_string_join(shortened, " ");
+      return {
+        input: `let value;
+value = '${joined}';
+console.log(value);`
+      };
+    }
+  };
 }
