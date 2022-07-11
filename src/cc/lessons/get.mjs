b65ff16987a459dbe438631dd1fec553bd050c76
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:15:40 -0400

    main noop added lesson

--- src/cc/lessons/get.mjs
+++ src/cc/lessons/get.mjs
@@ -1,6 +1,7 @@
 import {cc_lesson_console_log} from "./../lesson/console/log.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+import { cc_lesson_let_value } from "../lesson/let/value.mjs";
 export async function cc_lessons_get() {
   await m_js_arguments_assert()(arguments);
-  return [await cc_lesson_console_log()];
+  return [await cc_lesson_console_log(), await cc_lesson_let_value()];
 }
