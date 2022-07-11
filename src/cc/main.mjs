ecdc532f3cc90fe3210f733068cb5a42ce2a2be3
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:55:06 -0400

    main noop progress

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -15,7 +15,7 @@ export async function cc_main(parent) {
   await m_js_for_each(lessons, async lesson => {
     let li = await ui_element_text(list, "li", await m_js_property_get(lesson, "name"));
     li.style.listStyleType = "none";
-    await ui_element_style_border(li, 'black');
+    await ui_element_style_border(li, 'blue');
   });
 }
 await cc_main(document.body);
