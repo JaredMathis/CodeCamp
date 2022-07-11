f2a96d51734a7ba70f086ab08057ad2c87d439a0
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:09:51 -0400

    main noop progress

--- src/cc/lesson/let/value.mjs
+++ src/cc/lesson/let/value.mjs
@@ -1,4 +1,10 @@
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_let_value() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "let value = ...;",
+    example_get: async () => {
+
+    }
+  };
 }
