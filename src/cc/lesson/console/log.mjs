ce1cda8347945faad4fb5c86046b202d5f505acf
JaredMathis <jared.mathis@native.consulting>
2022-07-11 18:56:05 -0400

    main noop unused imports

--- src/cc/lesson/console/log.mjs
+++ src/cc/lesson/console/log.mjs
@@ -1,14 +1,8 @@
 import {random_list_slice} from "./../../../../node_modules/mykro/src/random/list/slice.mjs";
-import {list_take} from "./../../../../node_modules/mykro/src/list/take.mjs";
 import {m_js_string_join} from "./../../../../node_modules/mykro/src/m/js/string/join.mjs";
-import {list_join} from "./../../../../node_modules/mykro/src/list/join.mjs";
-import {list_starting_at} from "./../../../../node_modules/mykro/src/list/starting/at.mjs";
-import {random_number} from "./../../../../node_modules/mykro/src/random/number.mjs";
-import {list_size} from "./../../../../node_modules/mykro/src/list/size.mjs";
 import {m_js_string_split} from "./../../../../node_modules/mykro/src/m/js/string/split.mjs";
 import {list_map} from "./../../../../node_modules/mykro/src/list/map.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-import {list_max_index} from "./../../../../node_modules/mykro/src/list/max/index.mjs";
 export async function cc_lesson_console_log() {
   await m_js_arguments_assert()(arguments);
   let verse = `For God so loved the world that he gave his only Son so that everyone who believes in him may not perish but may have eternal life`;
