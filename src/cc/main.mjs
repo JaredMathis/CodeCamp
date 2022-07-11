3e1e59fac0519c4411924c162097569c7f9bb4dd
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:11:36 -0400

    main noop progress

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -3,9 +3,9 @@ import {ui_element} from "./../../node_modules/mykro/src/ui/element.mjs";
 import {m_js_arguments_assert} from "./../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 import {cc_lessons_get} from "./lessons/get.mjs";
 export async function cc_main() {
-  await m_js_arguments_assert()(arguments);
+  await m_js_arguments_assert(parent)(arguments);
   let lessons = await cc_lessons_get();
-  let span = await ui_element(document.body, "span");
+  let span = await ui_element(parent, "span");
   await ui_element_html_inner_set(span, "Lessons");
 }
-await cc_main();
+await cc_main(document.body);
