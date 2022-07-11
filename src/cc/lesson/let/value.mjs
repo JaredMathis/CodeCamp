acb713fb00231f3ebd2a9864945686b7db3352cc
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:59:59 -0400

    main file_js_auto cc_lesson_let_value

--- src/cc/lesson/let/value.mjs
+++ src/cc/lesson/let/value.mjs
@@ -1,6 +1,8 @@
 import {random_list_slice_join} from "./../../../../node_modules/mykro/src/random/list/slice/join.mjs";
 import {cc_tokens_get} from "./../../tokens/get.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+import {m_js_while_max} from "./../../../../node_modules/mykro/src/m/js/while/max.mjs";
+import {m_js_equals} from "mykro/src/m/js/equals.mjs";
 export async function cc_lesson_let_value() {
   await m_js_arguments_assert()(arguments);
   let tokens = await cc_tokens_get();
@@ -8,8 +10,16 @@ export async function cc_lesson_let_value() {
     name: "let value; value = ...;",
     example_get: async () => {
       let joined = await random_list_slice_join(tokens, 3, " ");
+      let other;
+      await m_js_while_max(100, async () => {
+        other = await random_list_slice_join(tokens, 3, " ");
+        if (!m_js_equals(joined, other)) {
+          return true;
+        }
+      });
       return {
         input: `let value;
+value = '${other}';
 value = '${joined}';
 console.log(value);`
       };
