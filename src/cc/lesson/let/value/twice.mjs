0f7f78a81ceae62e0998e7dfcd393c2b0437ce8e
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:57:27 -0400

    main noop unused imports

--- src/cc/lesson/let/value/twice.mjs
+++ src/cc/lesson/let/value/twice.mjs
@@ -1,6 +1,4 @@
 import {random_list_slice_join} from "./../../../../../node_modules/mykro/src/random/list/slice/join.mjs";
-import {m_js_string_join} from "./../../../../../node_modules/mykro/src/m/js/string/join.mjs";
-import {random_list_slice} from "./../../../../../node_modules/mykro/src/random/list/slice.mjs";
 import {cc_tokens_get} from "./../../../tokens/get.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_let_value_twice() {
