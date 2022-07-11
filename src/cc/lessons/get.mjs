eb35d351c8f87cbae088d75176f88e4008c96006
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:45:39 -0400

    main file_js_auto cc_lessons_get

--- src/cc/lessons/get.mjs
+++ src/cc/lessons/get.mjs
@@ -1,7 +1,8 @@
+import {cc_lesson_let_value_twice} from "./../lesson/let/value/twice.mjs";
 import {cc_lesson_console_log} from "./../lesson/console/log.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-import { cc_lesson_let_value } from "../lesson/let/value.mjs";
+import {cc_lesson_let_value} from "./../lesson/let/value.mjs";
 export async function cc_lessons_get() {
   await m_js_arguments_assert()(arguments);
-  return [await cc_lesson_console_log(), await cc_lesson_let_value()];
+  return [await cc_lesson_console_log(), await cc_lesson_let_value(), await cc_lesson_let_value_twice()];
 }
