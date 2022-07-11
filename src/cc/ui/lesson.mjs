1e57a124724b4352717a00ea3048941c61b14fa6
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:58:59 -0400

    main file_js_auto cc_ui_lesson_example

--- src/cc/ui/lesson.mjs
+++ src/cc/ui/lesson.mjs
@@ -15,7 +15,7 @@ export async function cc_ui_lesson(parent, lesson, go_back) {
   let span = await ui_element_text(container, "h1", "Lesson " + await m_js_property_get(lesson, "name"));
   let view = await ui_view(container);
   view.view_set(async (parent) => {
-    return await cc_ui_lesson_example(parent, lesson);
+    return await cc_ui_lesson_example(parent, lesson.examples[0]);
   });
   let back = await ui_element_text(container, "button", "Back to Lessons");
   await ui_element_style_button_secondary(back);
--- src/cc/ui/lesson/example.mjs
+++ src/cc/ui/lesson/example.mjs
@@ -1,12 +1,16 @@
+import {m_js_property_get} from "./../../../../node_modules/mykro/src/m/js/property/get.mjs";
 import {ui_element} from "./../../../../node_modules/mykro/src/ui/element.mjs";
 import {m_js_defined_is} from "./../../../../node_modules/mykro/src/m/js/defined/is.mjs";
 import {ui_html_element_is} from "./../../../../node_modules/mykro/src/ui/html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 import {ui_element_html_inner_set} from "./../../../../node_modules/mykro/src/ui/element/html/inner/set.mjs";
-export async function cc_ui_lesson_example(parent, lesson) {
+export async function cc_ui_lesson_example(parent, example) {
   await m_js_arguments_assert(ui_html_element_is, m_js_defined_is)(arguments);
   let container = await ui_element(parent, "div");
-  await ui_element_html_inner_set(container, "test");
+  let input = await ui_element(container, "div");
+  await ui_element_html_inner_set(input, await m_js_property_get(example, "input"));
+  let output = await ui_element(container, "div");
+  await ui_element_html_inner_set(container, await m_js_property_get(output, "output"));
   return {
     container
   };
