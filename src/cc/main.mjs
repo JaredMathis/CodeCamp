97de3dd2f0fb66f764fa2dedb5ab2c3e53d78f2b
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:29:00 -0400

    main noop missing await

--- src/cc/main.mjs
+++ src/cc/main.mjs
@@ -10,7 +10,7 @@ export async function cc_main(parent) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
   let lessons = await cc_lessons_get();
   let span = await ui_element_text(parent, "h1", "Lessons");
-  let list = ui_element(parent, "ul");
+  let list = await ui_element(parent, "ul");
   await m_js_for_each(lessons, async lesson => {
     await ui_element_text(list, "li", await m_js_property_get(lesson, "name"));
   });
