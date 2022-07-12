7b350185e998c7abde1d908d7d15744ba697476a
JaredMathis <jared.mathis@native.consulting>
2022-07-12 09:21:56 -0400

    main file_js_auto cc_lesson_let_value

--- src/cc/lesson/let/value.mjs
+++ src/cc/lesson/let/value.mjs
@@ -1,14 +1,14 @@
-import {cc_tokens_3_get} from "./../../tokens/3/get.mjs";
+import {cc_random_number_1_10} from "./../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_let_value() {
   await m_js_arguments_assert()(arguments);
   return {
     name: "let value; value = ...;",
     example_get: async () => {
-      let joined = await cc_tokens_3_get();
+      let x = await cc_random_number_1_10();
       return {
         input: `let value;
-value = '${joined}';
+value = ${x};
 console.log(value);`
       };
     }
