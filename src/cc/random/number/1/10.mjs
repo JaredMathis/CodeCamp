25e59723621cd8b87adc92187e179bb661dde1b7
JaredMathis <jared.mathis@native.consulting>
2022-07-12 09:18:59 -0400

    main file_js_auto cc_random_number_1_10

--- src/cc/random/number/1/10.mjs
+++ src/cc/random/number/1/10.mjs
@@ -1,4 +1,7 @@
+import {random_number} from "./../../../../../node_modules/mykro/src/random/number.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_random_number_1_10() {
   await m_js_arguments_assert()(arguments);
+  let x = await random_number(1, 10);
+  return x;
 }
