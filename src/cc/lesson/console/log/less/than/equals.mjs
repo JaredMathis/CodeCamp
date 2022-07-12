41c6580e1b57aae3a249006ec97e60c5cf060792
JaredMathis <jared.mathis@native.consulting>
2022-07-12 16:14:36 -0400

    main noop copied code

--- src/cc/lesson/console/log/less/than/equals.mjs
+++ src/cc/lesson/console/log/less/than/equals.mjs
@@ -1,4 +1,13 @@
 import {m_js_arguments_assert} from "./../../../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_console_log_less_than_equals() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "console.log(x);",
+    example_get: async () => {
+      let x = await cc_random_number_1_10();
+      return {
+        input: `console.log(${x});`
+      };
+    }
+  };
 }
