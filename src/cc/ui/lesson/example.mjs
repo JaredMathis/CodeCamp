99afd8c11a55a25737f609022f71291d91e97842
JaredMathis <jared.mathis@native.consulting>
2022-07-12 16:42:41 -0400

    main file_js_auto cc_ui_lesson_example

--- src/cc/ui/lesson/example.mjs
+++ src/cc/ui/lesson/example.mjs
@@ -1,3 +1,4 @@
+import {ui_element_html_inner_set} from "./../../../../node_modules/mykro/src/ui/element/html/inner/set.mjs";
 import {noop} from "./../../../../node_modules/mykro/src/noop.mjs";
 import {random_list_shuffle} from "./../../../../node_modules/mykro/src/random/list/shuffle.mjs";
 import {m_js_equals} from "./../../../../node_modules/mykro/src/m/js/equals.mjs";
@@ -18,7 +19,8 @@ import {list_max_index} from "./../../../../node_modules/mykro/src/list/max/inde
 import {m_js_property_has} from "./../../../../node_modules/mykro/src/m/js/property/has.mjs";
 import {list_add_all} from "./../../../../node_modules/mykro/src/list/add/all.mjs";
 import {ui_color_blue} from "./../../../../node_modules/mykro/src/ui/color/blue.mjs";
-import { cc_values_different_generate } from "../../values/different/generate.mjs";
+import {cc_values_different_generate} from "./../../values/different/generate.mjs";
+import {list_size} from "./../../../../node_modules/mykro/src/list/size.mjs";
 export async function cc_ui_lesson_example(parent, example_get, example_number, on_next, on_quiz_me, is_quiz) {
   await m_js_arguments_assert(ui_html_element_is, m_js_function_is, m_js_number_is, m_js_function_is, m_js_function_is, m_js_boolean_is)(arguments);
   let container = await ui_element(parent, "div");
@@ -35,7 +37,7 @@ export async function cc_ui_lesson_example(parent, example_get, example_number, 
   const input_text = await m_js_property_get(example, "input");
   let input_lines = await m_js_string_split(input_text, "\n");
   let input = await ui_element_text(container, "div", "");
-  await ui_element_lines_monospace(input, input_lines, line => line.style.color = "white");
+  await ui_element_lines_monospace(input, input_lines, line => line.style.color = "white", "TODO");
   await ui_element_style_background_color_border(input, "0,0,0", 1, 1);
   input.style.overflowWrap = "break-word";
   input.style.color = "white";
@@ -58,13 +60,13 @@ export async function cc_ui_lesson_example(parent, example_get, example_number, 
           await ui_element_style_background_color_border(answer_button, red, 0.5, 0.2);
         }
       });
-      await ui_element_lines_monospace(answer_button, await m_js_string_split(answer, "\n"), noop);
+      await ui_element_lines_monospace(answer_button, await m_js_string_split(answer, "\n"), noop, "(No output)");
     });
   } else {
     let next_text = `Another example, please!`;
     let output = await ui_element_text(container, "div", "");
     let green = "0,255,0";
-    await ui_element_lines_monospace(output, await m_js_string_split(answer_right, "\n"), noop);
+    await ui_element_lines_monospace(output, await m_js_string_split(answer_right, "\n"), noop, "(No output)");
     await ui_element_style_monospace(output);
     await ui_element_style_background_color_border(output, green, 0.5, 0.2);
     await ui_element_button_primary(container, next_text, on_next);
@@ -80,8 +82,11 @@ export async function cc_ui_lesson_example(parent, example_get, example_number, 
     return wrong_answer;
   }
 }
-
-async function ui_element_lines_monospace(container, lines, for_each_line) {
+async function ui_element_lines_monospace(container, lines, for_each_line, no_lines_message) {
+  if (m_js_equals(await list_size(lines), 0)) {
+    await ui_element_html_inner_set(container, no_lines_message);
+    container.style.fontStyle = "italic";
+  }
   await m_js_for_each(lines, async (line_text, index) => {
     let line = await ui_element_text(container, "div", line_text);
     await ui_element_style_monospace(line);
