e19320ce62cf7adc3735aa1963f7361e091895dd
JaredMathis <jared.mathis@native.consulting>
2022-07-12 09:20:41 -0400

    main file_js_auto cc_lesson_console_log_twice

--- src/cc/lesson/console/log/twice.mjs
+++ src/cc/lesson/console/log/twice.mjs
@@ -1,16 +1,16 @@
-import {cc_tokens_3_get} from "./../../../tokens/3/get.mjs";
+import {cc_random_number_1_10} from "./../../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_console_log_twice() {
   await m_js_arguments_assert()(arguments);
   return {
     name: "console.log('...'); // x2",
     example_get: async () => {
-      let joined = await cc_tokens_3_get();
-      let joined2 = await cc_tokens_3_get();
+      let x = await cc_random_number_1_10();
+      let y = await cc_random_number_1_10();
       return {
-        input: `console.log('${joined}');
-console.log('${joined2}');`,
-        answers_wrong: [joined2 + "\n" + joined, joined + " " + joined2]
+        input: `console.log(${x});
+console.log(${y});`,
+        answers_wrong: [y + "\n" + x, x + " " + y]
       };
     }
   };
