f79c7ecab8cc6cf7afb8cdf45b8ca9a4b0052b50
JaredMathis <jared.mathis@native.consulting>
2022-07-12 17:03:13 -0400

    main file_js_auto cc_lesson_if_false

--- src/cc/lesson/if/false.mjs
+++ src/cc/lesson/if/false.mjs
@@ -1,5 +1,5 @@
-import {cc_random_number_1_10} from "../../random/number/1/10.mjs";
-import {m_js_arguments_assert} from "mykro/src/m/js/arguments/assert.mjs";
+import {cc_random_number_1_10} from "./../../random/number/1/10.mjs";
+import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_if_false() {
   await m_js_arguments_assert()(arguments);
   return {
