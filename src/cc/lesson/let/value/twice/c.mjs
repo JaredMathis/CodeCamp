18010acbcd7c43433aa3018fbb45b709bdd9f0f2
JaredMathis <jared.mathis@native.consulting>
2022-07-12 14:07:28 -0400

    main file_js_auto cc_lesson_let_value_twice_c

--- src/cc/lesson/let/value/twice/c.mjs
+++ src/cc/lesson/let/value/twice/c.mjs
@@ -1,12 +1,12 @@
-import {cc_tokens_3_get} from "./../../../../tokens/3/get.mjs";
 import {cc_value_different_generate} from "./../../../../value/different/generate.mjs";
 import {m_js_arguments_assert} from "./../../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+import {cc_random_number_1_10} from "./../../../../random/number/1/10.mjs";
 export async function cc_lesson_let_value_twice_c() {
   await m_js_arguments_assert()(arguments);
   return {
-    name: "let value; value = ...; // x2",
+    name: "let value = x; value = y;",
     example_get: async () => {
-      let {x, y} = await cc_value_different_generate(cc_tokens_3_get);
+      let {x, y} = await cc_value_different_generate(cc_random_number_1_10);
       return {
         input: `let value = '${y}';
 value = '${x}';
