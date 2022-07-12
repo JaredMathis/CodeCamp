d3748caeb2e45db9e9ae4e45d38fbf8664976e54
JaredMathis <jared.mathis@native.consulting>
2022-07-12 13:57:26 -0400

    main file_js_auto cc_lesson_all

--- src/cc/lesson/all.mjs
+++ src/cc/lesson/all.mjs
@@ -1,3 +1,4 @@
+import {cc_lesson_let_value_b} from "./let/value/b.mjs";
 import {cc_lesson_number_add} from "./number/add.mjs";
 import {cc_lesson_console_log_number} from "./console/log/number.mjs";
 import {cc_lesson_console_log_twice} from "./console/log/twice.mjs";
@@ -10,11 +11,13 @@ import {cc_lesson_let_value_twice_b} from "./let/value/twice/b.mjs";
 export async function cc_lesson_all() {
   await m_js_arguments_assert()(arguments);
   let result = [];
+  cc_lesson_let_value_b;
   await list_add(result, await cc_lesson_console_log_number());
   await list_add(result, await cc_lesson_number_add());
   await list_add(result, await cc_lesson_console_log_string());
   await list_add(result, await cc_lesson_console_log_twice());
   await list_add(result, await cc_lesson_let_value());
+  await list_add(result, await cc_lesson_let_value_b());
   await list_add(result, await cc_lesson_let_value_twice_b());
   await list_add(result, await cc_lesson_let_value_twice());
   return result;
