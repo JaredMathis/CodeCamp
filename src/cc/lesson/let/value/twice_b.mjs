9fc92607045a9cf7dcc1768c960b9d36441a6534
JaredMathis <jared.mathis@native.consulting>
2022-07-12 09:33:33 -0400

    main file_js_auto cc_lesson_let_value_twice_b

--- src/cc/lesson/let/value/twice_b.mjs
+++ src/cc/lesson/let/value/twice_b.mjs
@@ -1,27 +1,19 @@
+import {cc_value_different_generate} from "./../../../value/different/generate.mjs";
 import {cc_random_number_1_10} from "./../../../random/number/1/10.mjs";
-import {m_js_equals} from "./../../../../../node_modules/mykro/src/m/js/equals.mjs";
-import {m_js_while_max} from "./../../../../../node_modules/mykro/src/m/js/while/max.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_let_value_twice_b() {
   await m_js_arguments_assert()(arguments);
   return {
     name: "let value; value = ...; console...; // x2",
     example_get: async () => {
-      let x = await cc_random_number_1_10();
-      let other;
-      await m_js_while_max(100, async () => {
-        other = await cc_random_number_1_10();
-        if (!m_js_equals(x, other)) {
-          return true;
-        }
-      });
+      let {x, y} = await cc_value_different_generate(cc_random_number_1_10);
       return {
         input: `let value;
-value = ${other};
+value = ${y};
 console.log(value);
 value = ${x};
 console.log(value);`,
-        answers_wrong: [x + "\n" + other, other + " " + x]
+        answers_wrong: [x + "\n" + y, y + " " + x]
       };
     }
   };
