b4a54bd5008cede01b762f0dda5e99de5c7f6d86
JaredMathis <jared.mathis@native.consulting>
2022-07-11 20:33:09 -0400

    main file_js_auto cc_lesson_console_log_twice

--- src/cc/lesson/console/log/twice.mjs
+++ src/cc/lesson/console/log/twice.mjs
@@ -1,4 +1,14 @@
+import {cc_tokens_3_get} from "./../../../tokens/3/get.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_console_log_twice() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "console.log(...);",
+    example_get: async () => {
+      let joined = await cc_tokens_3_get();
+      return {
+        input: `console.log('${joined}');`
+      };
+    }
+  };
 }
