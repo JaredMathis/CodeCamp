63bd048c9c82eb425240697e8d892197cecbc3bb
JaredMathis <jared.mathis@native.consulting>
2022-07-12 09:19:21 -0400

    main file_js_auto cc_lesson_console_log_number

--- src/cc/lesson/console/log/number.mjs
+++ src/cc/lesson/console/log/number.mjs
@@ -1,3 +1,4 @@
+import {cc_random_number_1_10} from "./../../../random/number/1/10.mjs";
 import {random_number} from "./../../../../../node_modules/mykro/src/random/number.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_console_log_number() {
@@ -5,7 +6,7 @@ export async function cc_lesson_console_log_number() {
   return {
     name: "console.log(x);",
     example_get: async () => {
-      let x = await random_number(1, 10);
+      let x = await cc_random_number_1_10();
       return {
         input: `console.log(${x});`
       };
