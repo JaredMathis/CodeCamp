f9cc9181480673b47cd57f99e27839fde9364845
JaredMathis <jared.mathis@native.consulting>
2022-07-11 20:05:00 -0400

    main noop unused code

--- src/cc/lesson/let/value/twice.mjs
+++ src/cc/lesson/let/value/twice.mjs
@@ -1,12 +1,9 @@
 import {cc_tokens_3_get} from "./../../../tokens/3/get.mjs";
 import {m_js_equals} from "./../../../../../node_modules/mykro/src/m/js/equals.mjs";
 import {m_js_while_max} from "./../../../../../node_modules/mykro/src/m/js/while/max.mjs";
-import {random_list_slice_join} from "./../../../../../node_modules/mykro/src/random/list/slice/join.mjs";
-import {cc_tokens_get} from "./../../../tokens/get.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_let_value_twice() {
   await m_js_arguments_assert()(arguments);
-  let tokens = await cc_tokens_get();
   return {
     name: "let value; value = ...; value = ...;",
     example_get: async () => {
