cdb17e85635753f472b0676436552dc1d5a2d388
JaredMathis <jared.mathis@native.consulting>
2022-07-12 18:45:12 -0400

    main file_js_auto cc_lesson_number_add_let_assign

--- src/cc/lesson/number/add/let/assign.mjs
+++ src/cc/lesson/number/add/let/assign.mjs
@@ -1,4 +1,17 @@
+import {cc_random_number_1_10} from "./../../../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_number_add_let_assign() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "let x,y...; x = x + y; console...",
+    example_get: async () => {
+      let x = await cc_random_number_1_10();
+      let y = await cc_random_number_1_10();
+      return {
+        input: `let x = ${x}, y = ${y};
+        x = x + y;
+        console.log(x);`
+      };
+    }
+  };
 }
--- src/cc/lesson/number/add/one.mjs
+++ src/cc/lesson/number/add/one.mjs
@@ -2,13 +2,12 @@ import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/j
 export async function cc_lesson_number_add_one() {
   await m_js_arguments_assert()(arguments);
   return {
-    name: "let x = ...; console.log(x + y);",
+    name: "let x = ...; x = x + 1; console...",
     example_get: async () => {
       let x = await cc_random_number_1_10();
-      let y = await cc_random_number_1_10();
       return {
         input: `let x = ${x};
-        x = x + ${y};
+        x = x + 1;
         console.log(x);`
       };
     }
