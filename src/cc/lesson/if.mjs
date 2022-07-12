1544476569eab4582b1da34a70e1fbdcb0594ea9
JaredMathis <jared.mathis@native.consulting>
2022-07-12 16:52:28 -0400

    main file_js_add cc_lesson_if_true_false

--- src/cc/lesson/if.mjs
+++ src/cc/lesson/if.mjs
@@ -3,13 +3,11 @@ import {cc_random_number_1_10} from "./../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_if() {
   await m_js_arguments_assert()(arguments);
-  let x = await cc_random_number_1_10();
-  let y = await random_number(x - 1, x + 1);
   return {
     name: "if (x <= y)",
     example_get: async () => {
-      let x = await cc_random_number_1_10();
-      let y = await random_number(x - 1, x + 1);
+      let x = (await cc_random_number_1_10()) + 1;
+      let y = await random_number(x - 2, x + 1);
       return {
         input: `let x = ${x};
         if (x <= ${y}) {
