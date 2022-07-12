5e55317b63088f685efeccec2341abf9a9c36604
JaredMathis <jared.mathis@native.consulting>
2022-07-12 16:15:50 -0400

    main file_js_auto cc_lesson_console_log_less_than_equals

--- src/cc/lesson/console/log/less/than/equals.mjs
+++ src/cc/lesson/console/log/less/than/equals.mjs
@@ -1,3 +1,5 @@
+import {cc_random_number_1_10} from "./../../../../../random/number/1/10.mjs";
+import {random_number} from "mykro/src/random/number.mjs";
 import {m_js_arguments_assert} from "./../../../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_console_log_less_than_equals() {
   await m_js_arguments_assert()(arguments);
@@ -5,8 +7,9 @@ export async function cc_lesson_console_log_less_than_equals() {
     name: "console.log(x);",
     example_get: async () => {
       let x = await cc_random_number_1_10();
+      let y = await random_number(x - 1, x + 1);
       return {
-        input: `console.log(${x});`
+        input: `console.log(${x} <= ${y});`
       };
     }
   };
