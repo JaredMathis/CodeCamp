9fb25a3d81d6c94ac00705b4f1b66d381247fd14
JaredMathis <jared.mathis@native.consulting>
2022-07-12 13:56:47 -0400

    main file_js_auto cc_lesson_let_value_b

--- src/cc/lesson/let/value/b.mjs
+++ src/cc/lesson/let/value/b.mjs
@@ -1,4 +1,15 @@
+import {cc_random_number_1_10} from "./../../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_let_value_b() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "let value = ...;",
+    example_get: async () => {
+      let x = await cc_random_number_1_10();
+      return {
+        input: `let value = ${x};
+console.log(value);`
+      };
+    }
+  };
 }
