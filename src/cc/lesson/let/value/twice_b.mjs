9f2f99367d6df5e2b8b58ddfff523e9d149caa21
JaredMathis <jared.mathis@native.consulting>
2022-07-12 09:24:12 -0400

    main file_js_auto cc_lesson_let_value_twice_b

--- src/cc/lesson/let/value/twice_b.mjs
+++ src/cc/lesson/let/value/twice_b.mjs
@@ -1,3 +1,4 @@
+import {cc_random_number_1_10} from "./../../../random/number/1/10.mjs";
 import {cc_tokens_3_get} from "./../../../tokens/3/get.mjs";
 import {m_js_equals} from "./../../../../../node_modules/mykro/src/m/js/equals.mjs";
 import {m_js_while_max} from "./../../../../../node_modules/mykro/src/m/js/while/max.mjs";
@@ -7,21 +8,21 @@ export async function cc_lesson_let_value_twice_b() {
   return {
     name: "let value; value = ...; console...; // x2",
     example_get: async () => {
-      let joined = await cc_tokens_3_get();
+      let x = await cc_random_number_1_10();
       let other;
       await m_js_while_max(100, async () => {
-        other = await cc_tokens_3_get();
-        if (!m_js_equals(joined, other)) {
+        other = await cc_random_number_1_10();
+        if (!m_js_equals(x, other)) {
           return true;
         }
       });
       return {
         input: `let value;
-value = '${other}';
+value = ${other};
 console.log(value);
-value = '${joined}';
+value = ${x};
 console.log(value);`,
-        answers_wrong: [joined + "\n" + other, other + " " + joined]
+        answers_wrong: [x + "\n" + other, other + " " + x]
       };
     }
   };
