9fcd33b80b77881aed3518269d4a5fbea4d59993
JaredMathis <jared.mathis@native.consulting>
2022-07-11 20:51:26 -0400

    main noop copied

---
+++ src/cc/lesson/let/value/twice_b.mjs
@@ -0,0 +1,27 @@
+import {cc_tokens_3_get} from "../../../tokens/3/get.mjs";
+import {m_js_equals} from "mykro/src/m/js/equals.mjs";
+import {m_js_while_max} from "mykro/src/m/js/while/max.mjs";
+import {m_js_arguments_assert} from "mykro/src/m/js/arguments/assert.mjs";
+export async function cc_lesson_let_value_twice_b() {
+  await m_js_arguments_assert()(arguments);
+  return {
+    name: "let value; value = ...; // x2",
+    example_get: async () => {
+      let joined = await cc_tokens_3_get();
+      let other;
+      await m_js_while_max(100, async () => {
+        other = await cc_tokens_3_get();
+        if (!m_js_equals(joined, other)) {
+          return true;
+        }
+      });
+      return {
+        input: `let value;
+value = '${other}';
+value = '${joined}';
+console.log(value);`,
+        answers_wrong: [other], 
+      };
+    }
+  };
+}
