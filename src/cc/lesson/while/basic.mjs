02274a41e0a20c011a525ed7b9cbf8c0f570ce42
JaredMathis <jared.mathis@native.consulting>
2022-07-12 18:22:59 -0400

    main file_js_auto cc_lesson_while_basic

--- src/cc/lesson/while/basic.mjs
+++ src/cc/lesson/while/basic.mjs
@@ -1,3 +1,4 @@
+import {cc_value_different_generate} from "./../../value/different/generate.mjs";
 import {random_list_shuffle} from "./../../../../node_modules/mykro/src/random/list/shuffle.mjs";
 import {cc_random_number_1_10} from "./../../random/number/1/10.mjs";
 import {random_number} from "./../../../../node_modules/mykro/src/random/number.mjs";
@@ -7,10 +8,18 @@ export async function cc_lesson_while_basic() {
   let x = await cc_random_number_1_10();
   let y = x + await random_number(0, 2);
   return {
-    input: `let x = ${x};
-    while (x <= ${y}) {
-      console.log(x);
-    }`,
-    answers_wrong: [""]
+    name: "let x = ...; while (...)",
+    example_get: async () => {
+      let {x, y} = await cc_value_different_generate(cc_random_number_1_10);
+      let list = [true, false];
+      await random_list_shuffle(list);
+      return {
+        input: `let x = ${x};
+        while (x <= ${y}) {
+          console.log(x);
+        }`,
+        answers_wrong: [""]
+      };
+    }
   };
 }
