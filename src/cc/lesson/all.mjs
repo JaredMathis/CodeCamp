0f9e65e0b932e9dfec7f336e00a2575aa64ab78a
JaredMathis <jared.mathis@native.consulting>
2022-07-12 16:23:41 -0400

    main file_js_auto cc_lesson_all

--- src/cc/lesson/all.mjs
+++ src/cc/lesson/all.mjs
@@ -1,3 +1,4 @@
+import {cc_lesson_console_log_less_than} from "./console/log/less/than.mjs";
 import {cc_lesson_let_value_twice_c} from "./let/value/twice/c.mjs";
 import {cc_lesson_let_value_b} from "./let/value/b.mjs";
 import {cc_lesson_number_add} from "./number/add.mjs";
@@ -9,7 +10,7 @@ import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/argu
 import {cc_lesson_let_value} from "./let/value.mjs";
 import {list_add} from "./../../../node_modules/mykro/src/list/add.mjs";
 import {cc_lesson_let_value_twice_b} from "./let/value/twice/b.mjs";
-import { cc_lesson_console_log_less_than_equals } from "./console/log/less/than/equals.mjs";
+import {cc_lesson_console_log_less_than_equals} from "./console/log/less/than/equals.mjs";
 export async function cc_lesson_all() {
   await m_js_arguments_assert()(arguments);
   let result = [];
@@ -23,5 +24,6 @@ export async function cc_lesson_all() {
   await list_add(result, await cc_lesson_let_value_twice());
   await list_add(result, await cc_lesson_let_value_twice_c());
   await list_add(result, await cc_lesson_console_log_less_than_equals());
+  await list_add(result, await cc_lesson_console_log_less_than());
   return result;
 }
