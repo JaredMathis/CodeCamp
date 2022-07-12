aa1ed58daa095a4581460dac268180a2568e5afa
JaredMathis <jared.mathis@native.consulting>
2022-07-11 20:01:08 -0400

    main file_js_auto cc_lesson_let_value_twice

--- src/cc/lesson/let/value/twice.mjs
+++ src/cc/lesson/let/value/twice.mjs
@@ -1,3 +1,5 @@
+import {m_js_equals} from "./../../../../../node_modules/mykro/src/m/js/equals.mjs";
+import {m_js_while_max} from "./../../../../../node_modules/mykro/src/m/js/while/max.mjs";
 import {random_list_slice_join} from "./../../../../../node_modules/mykro/src/random/list/slice/join.mjs";
 import {cc_tokens_get} from "./../../../tokens/get.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
