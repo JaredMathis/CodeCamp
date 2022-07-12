8f9a8783748e90477e8fdce512e3fc6084ddd0c5
JaredMathis <jared.mathis@native.consulting>
2022-07-12 13:44:08 -0400

    main file_js_auto cc_lesson_all

--- src/cc/lesson/all.mjs
+++ src/cc/lesson/all.mjs
@@ -1,3 +1,4 @@
+import {cc_lesson_number_add} from "./number/add.mjs";
 import {cc_lesson_console_log_number} from "./console/log/number.mjs";
 import {cc_lesson_console_log_twice} from "./console/log/twice.mjs";
 import {cc_lesson_let_value_twice} from "./let/value/twice.mjs";
@@ -10,6 +11,7 @@ export async function cc_lesson_all() {
   await m_js_arguments_assert()(arguments);
   let result = [];
   await list_add(result, await cc_lesson_console_log_number());
+  await list_add(result, await cc_lesson_number_add());
   await list_add(result, await cc_lesson_console_log_string());
   await list_add(result, await cc_lesson_console_log_twice());
   await list_add(result, await cc_lesson_let_value());
