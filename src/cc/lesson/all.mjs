9100905d587c3a8dbdb24ee99c64ad63a65267fd
JaredMathis <jared.mathis@native.consulting>
2022-07-12 18:55:08 -0400

    main file_js_rename cc_lesson_let_value cc_lesson_let_value_assign

--- src/cc/lesson/all.mjs
+++ src/cc/lesson/all.mjs
@@ -9,7 +9,7 @@ import {cc_lesson_console_log_twice} from "./console/log/twice.mjs";
 import {cc_lesson_let_value_twice} from "./let/value/twice.mjs";
 import {cc_lesson_console_log_string} from "./console/log/string.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-import {cc_lesson_let_value} from "./let/value.mjs";
+import {cc_lesson_let_value_assign} from "./let/value/assign.mjs";
 import {list_add} from "./../../../node_modules/mykro/src/list/add.mjs";
 import {cc_lesson_let_value_twice_b} from "./let/value/twice/b.mjs";
 import {cc_lesson_console_log_less_than_equals} from "./console/log/less/than/equals.mjs";
@@ -27,7 +27,7 @@ export async function cc_lesson_all() {
   await list_add(result, await cc_lesson_number_add());
   await list_add(result, await cc_lesson_console_log_string());
   await list_add(result, await cc_lesson_console_log_twice());
-  await list_add(result, await cc_lesson_let_value());
+  await list_add(result, await cc_lesson_let_value_assign());
   await list_add(result, await cc_lesson_let_value_twice_b());
   await list_add(result, await cc_lesson_let_value_b());
   await list_add(result, await cc_lesson_let_value_twice());
--- src/cc/lesson/let/value/assign.mjs
+++ src/cc/lesson/let/value/assign.mjs
@@ -1,6 +1,6 @@
-import {cc_random_number_1_10} from "./../../random/number/1/10.mjs";
-import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function cc_lesson_let_value() {
+import {cc_random_number_1_10} from "./../../../random/number/1/10.mjs";
+import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+export async function cc_lesson_let_value_assign() {
   await m_js_arguments_assert()(arguments);
   return {
     name: "let value; value = ...;",
