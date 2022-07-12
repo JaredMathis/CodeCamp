3d5cb63dd624daafa5a5eb76f835b819538fbe0f
JaredMathis <jared.mathis@native.consulting>
2022-07-11 20:03:13 -0400

    main file_js_auto cc_tokens_3_get

--- src/cc/tokens/3/get.mjs
+++ src/cc/tokens/3/get.mjs
@@ -1,4 +1,9 @@
+import {random_list_slice_join} from "./../../../../node_modules/mykro/src/random/list/slice/join.mjs";
+import {cc_tokens_get} from "./../get.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_tokens_3_get() {
   await m_js_arguments_assert()(arguments);
+  let tokens = await cc_tokens_get();
+  let joined = await random_list_slice_join(tokens, 3, " ");
+  return joined;
 }
