3107ae41d1e92488c8bb4ac3d5261d672e153d42
JaredMathis <jared.mathis@native.consulting>
2022-07-12 09:48:28 -0400

    main file_js_auto cc_values_different_generate

--- src/cc/values/different/generate.mjs
+++ src/cc/values/different/generate.mjs
@@ -3,9 +3,9 @@ import {list_contains} from "./../../../../node_modules/mykro/src/list/contains.
 import {list_size} from "./../../../../node_modules/mykro/src/list/size.mjs";
 import {m_js_number_at_least} from "./../../../../node_modules/mykro/src/m/js/number/at/least.mjs";
 import {m_js_while_max} from "./../../../../node_modules/mykro/src/m/js/while/max.mjs";
-import {list_is} from "mykro/src/list/is.mjs";
-import {m_js_function_is} from "mykro/src/m/js/function/is.mjs";
-import {m_js_number_is} from "mykro/src/m/js/number/is.mjs";
+import {list_is} from "./../../../../node_modules/mykro/src/list/is.mjs";
+import {m_js_function_is} from "./../../../../node_modules/mykro/src/m/js/function/is.mjs";
+import {m_js_number_is} from "./../../../../node_modules/mykro/src/m/js/number/is.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_values_different_generate(answers, answers_count, answer_get) {
   await m_js_arguments_assert(list_is, m_js_number_is, m_js_function_is)(arguments);
