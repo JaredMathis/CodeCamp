c1e9219c38e48357669bec9a0d5cdfcee1f0a6b2
JaredMathis <jared.mathis@native.consulting>
2022-07-11 21:45:52 -0400

    main file_js_auto cc_ui_lessons

--- src/cc/ui/lessons.mjs
+++ src/cc/ui/lessons.mjs
@@ -19,7 +19,7 @@ export async function cc_ui_lessons(parent, view) {
       await view.view_set(async () => await cc_ui_lesson(parent, lesson, async function go_back() {
         await view.view_set(async () => await cc_ui_lessons(parent, view));
       }, function next_lesson() {
-        alert('here')
+        alert("here");
       }));
     });
   });
