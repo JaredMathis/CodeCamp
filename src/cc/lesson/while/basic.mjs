9098acdd3cc9a8c6ca383edd085374d661b39322
JaredMathis <jared.mathis@native.consulting>
2022-07-12 19:27:17 -0400

    main noop progress

--- src/cc/lesson/while/basic.mjs
+++ src/cc/lesson/while/basic.mjs
@@ -1,5 +1,4 @@
 import {cc_random_number_1_10} from "./../../random/number/1/10.mjs";
-import {random_number} from "./../../../../node_modules/mykro/src/random/number.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_while_basic() {
   await m_js_arguments_assert()(arguments);
@@ -7,14 +6,14 @@ export async function cc_lesson_while_basic() {
     name: "let x = ...; while (...)",
     example_get: async () => {
       let x = await cc_random_number_1_10();
-      let y = x + await random_number(1, 2);
+      let y = x + 1;
       return {
         input: `let x = ${x};
         while (x <= ${y}) {
           console.log(x);
           x++;
         }`,
-        answers_wrong: [""]
+        answers_wrong: ["", `${x}`, `${x}\n${y}\n${y+1}`]
       };
     }
   };
