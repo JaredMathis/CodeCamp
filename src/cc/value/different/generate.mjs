7579f1414be003e785d6c14f8bf33b9f4053d5e7
JaredMathis <jared.mathis@native.consulting>
2022-07-12 09:32:09 -0400

    main file_js_auto cc_value_different_generate

--- src/cc/value/different/generate.mjs
+++ src/cc/value/different/generate.mjs
@@ -1,4 +1,20 @@
+import {m_js_equals} from "./../../../../node_modules/mykro/src/m/js/equals.mjs";
+import {cc_random_number_1_10} from "./../../random/number/1/10.mjs";
+import {m_js_while_max} from "./../../../../node_modules/mykro/src/m/js/while/max.mjs";
+import {m_js_function_is} from "./../../../../node_modules/mykro/src/m/js/function/is.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function cc_value_different_generate() {
-  await m_js_arguments_assert()(arguments);
+export async function cc_value_different_generate(value_get) {
+  await m_js_arguments_assert(m_js_function_is)(arguments);
+  let x = await value_get();
+  let y;
+  await m_js_while_max(100, async () => {
+    scrollY = await cc_random_number_1_10();
+    if (!m_js_equals(x, other)) {
+      return true;
+    }
+  });
+  return {
+    x,
+    y
+  };
 }
