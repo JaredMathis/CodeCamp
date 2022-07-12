72041c8ea17dadbe66c4a20ab15fee89efc90452
JaredMathis <jared.mathis@native.consulting>
2022-07-12 08:48:07 -0400

    main file_js_auto cc_lesson_console_log_number

--- src/cc/lesson/console/log/number.mjs
+++ src/cc/lesson/console/log/number.mjs
@@ -1,4 +1,14 @@
+import {random_number} from "./../../../../../node_modules/mykro/src/random/number.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_console_log_number() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "console.log(x);",
+    example_get: async () => {
+      let joined = random_number();
+      return {
+        input: `console.log(${joined});`
+      };
+    }
+  };
 }
