f02650a0900ddf2e82b25001c8f4b0a21be7f729
JaredMathis <jared.mathis@native.consulting>
2022-07-13 14:45:52 -0400

    main file_js_auto ui_element_card

--- src/ui/element/card.mjs
+++ src/ui/element/card.mjs
@@ -1,3 +1,6 @@
+import {ui_element_style_background_color_border} from "./../../../node_modules/mykro/src/ui/element/style/background/color/border.mjs";
+import {ui_element} from "./../../../node_modules/mykro/src/ui/element.mjs";
+import {ui_html_element_is} from "./../../../node_modules/mykro/src/ui/html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function ui_element_card(parent) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
