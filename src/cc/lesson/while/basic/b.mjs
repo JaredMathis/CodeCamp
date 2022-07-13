9c93ccc94290d3263512cae8ab51ef61b03fae4a
JaredMathis <jared.mathis@native.consulting>
2022-07-12 23:02:25 -0400

    main noop progress

--- src/cc/lesson/while/basic/b.mjs
+++ src/cc/lesson/while/basic/b.mjs
@@ -3,7 +3,7 @@ import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/j
 export async function cc_lesson_while_basic_b() {
   await m_js_arguments_assert()(arguments);
   return {
-    name: "let x = ...; while (...)",
+    name: "let x = ...; while (...) // (2)",
     example_get: async () => {
       let x = await cc_random_number_1_10();
       let y = x + 2;
