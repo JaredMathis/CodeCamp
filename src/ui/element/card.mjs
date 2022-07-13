746c3ce7f96568ae79d61fb3ccdeab68fee403fc
JaredMathis <jared.mathis@native.consulting>
2022-07-13 14:45:37 -0400

    main file_js_argument_add argadd ui_element_card parent ui_html_element_is

--- src/ui/element/card.mjs
+++ src/ui/element/card.mjs
@@ -1,4 +1,7 @@
 import {m_js_arguments_assert} from "./../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function ui_element_card() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_element_card(parent) {
+  await m_js_arguments_assert(ui_html_element_is)(arguments);
+  let container = await ui_element(parent, "div");
+  await ui_element_style_background_color_border(container, "1,1,1", 0.1, 0.03);
+  return container;
 }
