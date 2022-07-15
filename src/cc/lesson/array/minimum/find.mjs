9780ab00439855c94fe7901fdf3ca8af315e0ee6
JaredMathis <jared.mathis@native.consulting>
2022-07-15 15:05:02 -0400

    main file_js_auto cc_lesson_array_minimum_find

--- src/cc/lesson/array/minimum/find.mjs
+++ src/cc/lesson/array/minimum/find.mjs
@@ -1,4 +1,16 @@
+import {cc_values_different_generate} from "./../../../values/different/generate.mjs";
+import {cc_random_number_1_10} from "./../../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_array_minimum_find() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "console.log(x);",
+    example_get: async () => {
+      let x = await cc_values_different_generate(cc_random_number_1_10);
+      return {
+        input: `let list = [];
+        `
+      };
+    }
+  };
 }
