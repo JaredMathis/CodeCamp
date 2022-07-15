014b5220072251ff33053a22cc568a3edea991ee
JaredMathis <jared.mathis@native.consulting>
2022-07-15 16:21:51 -0400

    main file_js_auto cc_lesson_array_smallest

--- src/cc/lesson/array/smallest.mjs
+++ src/cc/lesson/array/smallest.mjs
@@ -2,7 +2,7 @@ import {list_smallest} from "./../../../../node_modules/mykro/src/list/smallest.
 import {list_without} from "./../../../../node_modules/mykro/src/list/without.mjs";
 import {cc_values_different_generate} from "./../../values/different/generate.mjs";
 import {cc_random_number_1_10} from "./../../random/number/1/10.mjs";
-import {m_js_arguments_assert} from "mykro/src/m/js/arguments/assert.mjs";
+import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_array_smallest() {
   await m_js_arguments_assert()(arguments);
   return {
