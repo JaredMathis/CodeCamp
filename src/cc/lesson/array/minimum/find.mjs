1a997a1029fc836f8bee3314eae0d78bed33554e
JaredMathis <jared.mathis@native.consulting>
2022-07-15 15:03:21 -0400

    main file_js_add cc_lesson_array_minimum_find

---
+++ src/cc/lesson/array/minimum/find.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+export async function cc_lesson_array_minimum_find() {
+  await m_js_arguments_assert()(arguments);
+}
