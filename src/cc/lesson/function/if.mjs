3416c0ffaa3bfd3020ba3781c87305074059e75f
JaredMathis <jared.mathis@native.consulting>
2022-07-15 23:45:03 -0400

    main file_js_auto cc_lesson_function_if

--- src/cc/lesson/function/if.mjs
+++ src/cc/lesson/function/if.mjs
@@ -1,4 +1,19 @@
+import {cc_random_number_1_10} from "./../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_function_if() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "function log_value_bigger",
+    example_get: async () => {
+      let x = await cc_random_number_1_10();
+      let y = await cc_random_number_1_10();
+      return {
+        input: `function log_value_bigger() {
+          console.log(${x} > ${x});
+        }
+        console.log(log_value_bigger());`,
+        answers_wrong: [`${x}`, `${y}`]
+      };
+    }
+  };
 }
