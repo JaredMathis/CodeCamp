8de51381df1ae156db2b2859b37bd12144fc8729
JaredMathis <jared.mathis@native.consulting>
2022-07-15 23:08:39 -0400

    main file_js_auto cc_lesson_function_return_argument

--- src/cc/lesson/function/return/argument.mjs
+++ src/cc/lesson/function/return/argument.mjs
@@ -1,4 +1,18 @@
+import {cc_random_number_1_10} from "./../../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_function_return_argument() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "function identity",
+    example_get: async () => {
+      let x = await cc_random_number_1_10();
+      return {
+        input: `function identity(a) {
+          return a;
+        }
+        console.log(identity(${x}));`,
+        answers_wrong: [`${x}`]
+      };
+    }
+  };
 }
