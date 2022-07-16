3da9b81e4767320b5fcab983b4d0eec336a6f2e0
JaredMathis <jared.mathis@native.consulting>
2022-07-15 23:15:29 -0400

    main file_js_auto cc_lesson_function_return_argument_multiple

--- src/cc/lesson/function/return/argument/multiple.mjs
+++ src/cc/lesson/function/return/argument/multiple.mjs
@@ -1,4 +1,19 @@
+import {cc_random_number_1_10} from "./../../../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_function_return_argument_multiple() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "function add_one",
+    example_get: async () => {
+      let x = await cc_random_number_1_10();
+      let y = await cc_random_number_1_10();
+      return {
+        input: `function sum(a, b) {
+          return a + b;
+        }
+        console.log(sum(${x}, ${y}));`,
+        answers_wrong: [`${x}`, `${y}`]
+      };
+    }
+  };
 }
