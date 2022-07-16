224133df7a0ff331cae01f304eb7bb8282669bcf
JaredMathis <jared.mathis@native.consulting>
2022-07-15 22:06:55 -0400

    main file_js_add ui_element_lines_monospace

--- src/cc/ui/element/code.mjs
+++ src/cc/ui/element/code.mjs
@@ -6,7 +6,7 @@ import {m_js_string_is} from "./../../../../node_modules/mykro/src/m/js/string/i
 import {ui_html_element_is} from "./../../../../node_modules/mykro/src/ui/html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_ui_element_code(parent, title_text, code_text) {
-  await m_js_arguments_assert(ui_html_element_is, m_js_string_is)(arguments);
+  await m_js_arguments_assert(ui_html_element_is, m_js_string_is, m_js_string_is)(arguments);
   let container_input = await ui_element_card(parent);
   await ui_element_text(container_input, "div", title_text);
   let code_lines = await m_js_string_split(code_text, "\n");
---
+++ src/ui/element/lines/monospace.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+export async function ui_element_lines_monospace() {
+  await m_js_arguments_assert()(arguments);
+}
