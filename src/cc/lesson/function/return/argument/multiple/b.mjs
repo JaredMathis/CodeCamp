32a838c03868ae8db2aa38c5e6992ca116ac7e3b
JaredMathis <jared.mathis@native.consulting>
2022-07-16 00:35:26 -0400

    main file_js_add cc_lesson_function_return_argument_multiple_b

---
+++ src/cc/lesson/function/return/argument/multiple/b.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+export async function cc_lesson_function_return_argument_multiple_b() {
+  await m_js_arguments_assert()(arguments);
+}
