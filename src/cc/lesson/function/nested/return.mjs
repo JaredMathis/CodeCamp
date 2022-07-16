b3c0abc1be7a6fe2ee70b61f3552f80b73242109
JaredMathis <jared.mathis@native.consulting>
2022-07-16 00:31:26 -0400

    main file_js_auto cc_lesson_function_nested_return

--- src/cc/lesson/function/nested/return.mjs
+++ src/cc/lesson/function/nested/return.mjs
@@ -1,3 +1,4 @@
+import {cc_random_number_1_10} from "./../../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_function_nested_return() {
   await m_js_arguments_assert()(arguments);
