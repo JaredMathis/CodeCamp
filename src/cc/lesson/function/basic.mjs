272b17886339fdaeb209255e7e8cff817d55ece7
JaredMathis <jared.mathis@native.consulting>
2022-07-15 22:41:35 -0400

    main file_js_auto cc_lesson_function_basic

--- src/cc/lesson/function/basic.mjs
+++ src/cc/lesson/function/basic.mjs
@@ -1,4 +1,17 @@
+import {cc_random_number_1_10} from "./../../random/number/1/10.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function cc_lesson_function_basic() {
   await m_js_arguments_assert()(arguments);
+  return {
+    name: "function log",
+    example_get: async () => {
+      let x = await cc_random_number_1_10();
+      return {
+        input: `function log() {
+          console.log(${x});
+        }`,
+        answers_wrong: [`${x}`]
+      };
+    }
+  };
 }
